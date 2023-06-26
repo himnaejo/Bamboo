928ce6a0ad3c4a8b877a7472bd41cba90a8c2087
HyunseoY <maricat0715@gmail.com>
2023-06-26 22:12:04 +0900

    마이 피드 UI 프로토타입 완성

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,17 +1,39 @@
 import { styled } from "styled-components";
 import "style/color.css";
-import bamboo_logo from "asset/bamboo_logo.png";
+import bamboo_logo from "assets/bamboo_logo.png";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { auth } from "firebase.js";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 
 const Header = () => {
+  const [user, setUser] = useState();
+
+  const logOut = async event => {
+    event.preventDefault();
+    await signOut(auth);
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      setUser(user);
+    });
+  }, []);
+
   return (
     <StHeader>
       <div>
-        <Logo src={bamboo_logo} alt="로고이미지" />
-        <h1>BAMBOO</h1>
+        <Logo src={bamboo_logo} alt="" />
+        <h1>&lt;&gt;Bamboo&lt;&#47;&gt;</h1>
       </div>
       <Link to={"signin"}>
-        <Btn>로그인</Btn>
+        {user === null ? (
+          <Btn position={"header"}>로그인</Btn>
+        ) : (
+          <Btn position={"header"} onClick={logOut}>
+            로그아웃
+          </Btn>
+        )}
       </Link>
     </StHeader>
   );
@@ -22,7 +44,7 @@ export default Header;
 const Logo = styled.img`
   width: 50px;
   height: 50px;
-  margin-left: 5px;
+  margin-left: 23px;
 `;
 
 const StHeader = styled.header`
